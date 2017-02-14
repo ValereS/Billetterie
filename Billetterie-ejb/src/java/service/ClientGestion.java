4d0da587f9c3f13b018ef99de7e2890ded408fe1
cdi505 <cdi505@pltz898.AFPA-PARIS-DI>
2017-02-14 14:00:21 +0100

    Add ClientGestion session.

---
+++ Billetterie-ejb/src/java/service/ClientGestion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author cdi505
+ */
+@Stateless
+public class ClientGestion implements ClientGestionLocal {
+    @PersistenceContext(unitName = "Billetterie-ejbPU")
+    private EntityManager em;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    
+}
