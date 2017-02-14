171076bac510756a03f01582628c500f2679b75b
cdi505 <cdi505@pltz898.AFPA-PARIS-DI>
2017-02-14 15:10:47 +0100

    Client stuff

--- Billetterie-ejb/src/java/entities/Client.java
+++ Billetterie-ejb/src/java/entities/Client.java
@@ -5,11 +5,15 @@
  */
 package entities;
 
+import enums.CiviliteClient;
+import enums.StatutClient;
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -17,12 +21,20 @@ import javax.persistence.Id;
  */
 @Entity
 public class Client implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long code;
+    private CiviliteClient civilite;
     private String nom;
     private String prenom;
+    private String email;
+    private String hashMotPasse;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateNaissance;
+    private StatutClient statut;
+    private String commentaire;
 
     public Long getCode() {
         return code;
@@ -30,6 +42,14 @@ public class Client implements Serializable {
 
     public void setCode(Long id) {
         this.code = id;
+    }
+
+    public CiviliteClient getCivilite() {
+        return civilite;
+    }
+
+    public void setCivilite(CiviliteClient civilite) {
+        this.civilite = civilite;
     }
 
     public String getNom() {
@@ -46,6 +66,46 @@ public class Client implements Serializable {
 
     public void setPrenom(String prenom) {
         this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashMotPasse() {
+        return hashMotPasse;
+    }
+
+    public void setHashMotPasse(String hashMotPasse) {
+        this.hashMotPasse = hashMotPasse;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public StatutClient getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutClient statut) {
+        this.statut = statut;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
     }
 
     @Override
@@ -72,5 +132,5 @@ public class Client implements Serializable {
     public String toString() {
         return "entities.Client[ id=" + code + " ]";
     }
-    
+
 }
