1b34de803ed62e5c117e2785e93fa62ca2ed79dd
cdi508 <cdi508@pltz892.AFPA-PARIS-DI>
2017-02-14 12:56:56 +0100

    Create tables

---
+++ Billetterie-ejb/src/java/entities/Place.java
@@ -0,0 +1,69 @@
+
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Place implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numero;
+    private String rang;
+    private String description;
+
+    public Place() {
+    }
+
+    public Place(Long id, String numero, String rang, String description) {
+        this.id = id;
+        this.numero = numero;
+        this.rang = rang;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getRang() {
+        return rang;
+    }
+
+    public void setRang(String rang) {
+        this.rang = rang;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "entities.Place[ id=" + id + " ]";
+    }
+    
+}
