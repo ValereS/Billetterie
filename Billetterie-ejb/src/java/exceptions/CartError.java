5f92501b27cf0d58c44526a1a58d31f988ae0d64
philong <philong.do@gmail.com>
2017-03-01 13:49:02 +0100

    More work on shopping cart management

---
+++ Billetterie-ejb/src/java/exceptions/CartError.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author cdi505
+ */
+public class CartError extends Exception {
+
+    /**
+     * Creates a new instance of <code>CartError</code> without detail message.
+     */
+    public CartError() {
+    }
+
+    /**
+     * Constructs an instance of <code>CartError</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public CartError(String msg) {
+        super(msg);
+    }
+}
